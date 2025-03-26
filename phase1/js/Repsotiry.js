29337c63b290f8bf9e803158582e31f68bdf5f92
abdalrehmanqu <ad2203615@qu.edu.qa>
2025-03-26 16:35:03 +0300

    ..

--- phase1/js/Repsotiry.js
+++ phase1/js/Repsotiry.js
@@ -40,33 +40,36 @@ async function getInstructors() {
 
 // Testing course 
 let courseT={
-  "name": "Data Structures",
-  "credit": 4,
-  "courseNo": "CMPS 303",
-  "category": "lecture",
-  "college": "Computer Science",
+  "name": "Experimental General Physics for Engineering II",
+  "credit": 1,
+  "courseNo": "PHYS 194",
+  "category": "lab",
+  "college": "Physics",
   "sections": [
+  {
+  "sectionID": "B02",
+  "instructorID": "135",
+  "place": "C242",
+  "timing": "9:00-10:00",
+  "dow": "sun/tue/thu",
+  "campus": "female",
+  "status": "approved",
+  "students": [
     {
-      "sectionID": "L01",
-      "instructorID": "134",
-      "place": "C237",
-      "timing": "9:00-10:00",
-      "dow": "sun/tue/thu",
-      "campus": "male",
-      "status": "approved",
-      "studentIDs": [
-        {
-          "85": "A"
-        },
-        {
-          "86": "B"
-        },
-        {
-          "87": "B+"
-        }
-      ]
+      "id": "100",
+      "grade": "A-"
+    },
+    {
+      "id": "101",
+      "grade": "B"
+    },
+    {
+      "id": "102",
+      "grade": "B"
     }
   ]
+}
+]
 }
 
 // Add a new course
@@ -81,66 +84,6 @@ async function addCourse(course) {
     fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
 }
 
-// Add a section
-async function addSection(course) {
-    // Courses list
-    const courses= await getCourses();
-    
-    // Check if the course exist 
-    if (!courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category)){
-        addCourse(course);
-    }else{
-        const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
-        sections.push(...course.sections);
-    }
-
-    // update json file
-    fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
-}
-
-// Delete section
-async function deleteSection(course){
-    // Courses list
-    const courses= await getCourses();
-
-    // find sections list
-    const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
-    
-    // section index
-    const sectionIndex=sections.findIndex((s)=>s.sectionID==course.sections.sectionID);
-
-    //delete from sections list 
-    sections.splice(sectionIndex,1);
-
-    // update json file
-    fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8'); 
-}
-
-// Approve section
-async function approveSection(course) {
-    // Courses list
-    const courses= await getCourses();
-
-    // find section list
-    const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
-
-    // find section
-    const section = sections.find((s)=> s.sectionID==course.sections[0].sectionID);
-    
-    //new status value
-    section.status="approved";
-
-    // update json file
-    fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
-}
-
-// Find section
-async function findSection(course) {
-     
-
-      // Return section
-      return section;
-}
 
 
 ///           STUDENT           ///
@@ -199,6 +142,119 @@ async function WithdrawCourse(id,course) {
    // update json file
    fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
    fs.writeFileSync(studentFilePath, JSON.stringify(students, null, 2), 'utf8');
+}
+
+
+
+///           INSTRUCTOR            ///
+
+// Submit grade
+async function submitGrade(id,course,grade) {
+  // Student list
+  const students=await getStudents();
+    
+  // Find student by id
+  let student=students.find((s)=>s.id==id);
+
+  // Courses list
+  const courses= await getCourses();
+
+  // find section list
+  const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
+
+  // find section
+  let section = sections.find((s)=> s.sectionID==course.sections[0].sectionID);
+  
+  // Add grade to section
+  section.students.find((s)=> s.id==id).grade=grade;
+
+  // Add grade to student
+  student.currentSections.find((s)=>s.courseNo==course.courseNo && s.section==section.sectionID).grade=grade;
+
+  // update json file
+  fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
+  fs.writeFileSync(studentFilePath, JSON.stringify(students, null, 2), 'utf8');
+}
+
+
+
+///           ADMIN           ///
+
+// Add section
+async function addSection(course) {
+  // Courses list
+  const courses= await getCourses();
+
+  // Instructors list / admin list
+  const instructors=await getInstructors();
+  const admins=await getAdmins();
+  
+  // Find instructor
+  const instructor= instructors.find((i)=> i.id==course.instructorID) || admins.find((a)=> a.id==course.instructorID);
+
+  // Add section to instructor
+  instructor.sections.push({"courseNo":course.courseNo,"section":course.sections[0].sectionID});
+
+  // Check if the course exist 
+  if (!courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category)){
+      addCourse(course);
+  }else{
+      const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
+      sections.push(...course.sections);
+  }
+
+  // update json file
+  fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
+}
+
+// Delete section
+async function deleteSection(course){
+  // Courses list
+  const courses= await getCourses();
+
+  // find sections list
+  const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
+  
+  // section index
+  const sectionIndex=sections.findIndex((s)=>s.sectionID==course.sections.sectionID);
+
+  //delete from sections list 
+  sections.splice(sectionIndex,1);
+
+  // update json file
+  fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8'); 
+}
+
+// Approve section
+async function approveSection(course) {
+  // Courses list
+  const courses= await getCourses();
+
+  // find section list
+  const sections=courses.find((c)=>c.courseNo==course.courseNo && c.category==course.category).sections;
+
+  // find section
+  const section = sections.find((s)=> s.sectionID==course.sections[0].sectionID);
+  
+  // All students list
+  const students=await getStudents();
+    
+  // Find students have this course pendding
+  let student=students.filter((s)=> s.pendingSections.find((s)=> s.courseNo==course.courseNo && s.section==section.sectionID));
+
+  //new status value
+  section.status="approved";
+
+  // Transfrom the course from pendding list to current
+  student.forEach((temp)=> {
+    let index=temp.pendingSections.findIndex((s)=> s.courseNo==course.courseNo && s.section==section.sectionID);
+    temp.currentSections.push({...temp.pendingSections[index],"grade":""});
+    temp.pendingSections.splice(index,1);
+  })
+
+  // update json file
+  fs.writeFileSync(courseFilePath, JSON.stringify(courses, null, 2), 'utf8');
+  fs.writeFileSync(studentFilePath, JSON.stringify(students, null, 2), 'utf8');
 }
 
 
