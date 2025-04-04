f64976ac3a40e5f0b6f2992604069bf127ddf956
abdalrehmanqu <ad2203615@qu.edu.qa>
2025-04-04 18:40:26 +0300

    ,,,..

--- phase1/js/login.js
+++ phase1/js/login.js
@@ -1,17 +1,12 @@
-import {getUser} from './Repsotiry.js';
 
 // Validate login when clicking the btn
-document.getElementById("Login-btn").addEventListener("click",console.log("1111"));
+// document.getElementById("Login-btn").addEventListener("click",console.log("1111"));
 
 //login validation
 function login(){
     // Get email and pass from user input
     const email=document.getElementById("user-email").value;
     const pass=document.getElementById("user-password").value;
-
-    // get user info 
-    getUser(email,pass);
-
 }
 
-console.log(localStorage.user);
+console.log("login function called");
