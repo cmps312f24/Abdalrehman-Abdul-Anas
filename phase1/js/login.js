b3c5e2f137c562b2ed3acf84815f29f4b4cca9ff
abdalrehmanqu <ad2203615@qu.edu.qa>
2025-04-04 18:54:00 +0300

    ..

--- phase1/js/login.js
+++ phase1/js/login.js
@@ -1,12 +1,13 @@
 
 // Validate login when clicking the btn
-// document.getElementById("Login-btn").addEventListener("click",console.log("1111"));
+document.getElementById("Login-btn").addEventListener("click",login);
 
 //login validation
 function login(){
     // Get email and pass from user input
     const email=document.getElementById("user-email").value;
     const pass=document.getElementById("user-password").value;
+    console.log("Login button clicked");
 }
 
-console.log("login function called");
+
