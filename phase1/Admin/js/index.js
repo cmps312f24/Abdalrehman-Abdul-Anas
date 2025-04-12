f3e63a8d4f4a016ea19581e65e3066c1e5aba267
abdalrehmanqu <ad2203615@qu.edu.qa>
2025-04-12 17:47:41 +0300

    ..

--- phase1/Admin/js/index.js
+++ phase1/Admin/js/index.js
@@ -439,10 +439,12 @@ async function completeCourse(courseNo, sectionID) {
         const grade=gradeMap[student.sections.find(s=> s.courseNo == courseNo).grade];
         const currentgpa=student.gpa[student.gpa.length-1];
         const credit=sectionID[0]!="B"&&course.credit!=3? currentgpa.CH+course.credit : currentgpa.CH;
-        student.gpa.push({
+
+        credit!=currentgpa.CH? student.gpa.push({
             "CH": credit,
-            "gpa": credit!=currentgpa.CH? (currentgpa.gpa+grade)/(credit) : currentgpa.gpa
-        })
+            "gpa": (currentgpa.gpa+grade)/(credit)
+        }):"";
+        
         await fetch(baseUrl + `students/${std.id}`, {
             method: 'PUT',
             headers: {
